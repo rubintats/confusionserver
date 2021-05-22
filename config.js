2beb12a676476cd7744b46747d9ba4e2677a78d3
Tatyana Rubin <rubintats@gmail.com>
2021-05-22 16:24:18 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+  	'facebook': {
+        clientId: '585761525719697',
+        clientSecret: '33c08d6e5dc2a39ae3b81ee8cfedde9d'
+    }
 }
